0bd599cba83219038ee79e2591122a47c1258aba
Efetobor Obominuru <efetobor.obominuru@Interswitchng.com>
2019-08-22 19:16:18 +0100

    Updated Email to fg@th.com for ProfileTestSuite

--- sqlscript.sql
+++ sqlscript.sql
@@ -1,12 +1,13 @@
-USE uat_quickteller
+use uat_quickteller
+
 Go
 
-delete FROM [uat_quickteller].[dbo].[PasswordHistory] where Username='for@gh.com'
+delete FROM [uat_quickteller].[dbo].[PasswordHistory] where Username='fg@th.com'
 
 update [uat_quickteller].[dbo].[aspnet_Membership] set Password ='/L7YRlO31xK6z3VZtvADWN3QO/E=', PasswordSalt='Oi7hjWhTachda2mu28mL3A=='
-  				where Email = 'for@gh.com'
+  				where Email = 'fg@th.com'
 
-update [uat_quickteller].[dbo].[aspnet_Membership] set FailedPasswordAttemptCount ='0', IsLockedOut='0' where Email = 'for@gh.com'
+update [uat_quickteller].[dbo].[aspnet_Membership] set FailedPasswordAttemptCount ='0', IsLockedOut='0' where Email = 'fg@th.com'
 
 
 update [uat_quickteller].[dbo].[aspnet_Membership] set FailedPasswordAttemptCount ='0', IsLockedOut='0' where Email = 'abc@test.com'
